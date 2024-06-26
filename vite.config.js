ea037f97cdd1a3f899d2bcf590a36ab9ad45fd50
mateus <mateuslimazeredo@hotmail.com>
2024-06-26 00:42:54 -0300

    config cors

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { ghPages } from 'vite-plugin-gh-pages';
+const express = require('express');
+const { createServer: createViteServer } = require('vite');
+const cors = require('cors');
 
-export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/', // Substitua por '/nome-do-repositorio/'
-});
+async function createServer() {
+  const app = express();
+
+  // Configurações de CORS
+  const corsOptions = {
+    origin: 'https://cabeloeart.vercel.app', // substitua pelo seu domínio
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Content-Disposition'],
+    credentials: true,
+    optionsSuccessStatus: 204
+  };
+
+  app.use(cors(corsOptions));
+
+  // Crie o servidor Vite
+  const vite = await createViteServer({
+    server: { middlewareMode: 'html' }
+  });
+
+  // Use o middleware do Vite
+  app.use(vite.middlewares);
+
+  app.listen(3000, () => {
+    console.log('Server is running at http://localhost:3000');
+  });
+}
+
+createServer();
